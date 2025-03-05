3f6a9fa1d41d844026a3a8e714f7345b190d7e4b
Vastia <92559779+laivastia@users.noreply.github.com>
2025-03-05 10:07:47 +0900

    setting: 라이브러리 종속성 문제 해결, 모듈 절대 경로 설정, vscode 자동완성 기능 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }]
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components')
+    }
   }
 })
